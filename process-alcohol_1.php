0e751ea9150e0998d9d2dc34382c926d449ee7ec
K. Lieber <90074187+klieber-tech@users.noreply.github.com>
2023-11-19 19:08:10 -0600

    XAMPP 1 - Finally a working localhost
    XAMPP files using the Cocktail Variations to explore setup.

---
+++ process-alcohol_1.php
@@ -0,0 +1,44 @@
+<?php
+// Enable error reporting for debugging
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect and sanitize input data
+    $alcoholType = htmlspecialchars($_POST['alcohol']);
+
+    // Specify the path to the XML file
+    $xmlFilePath = 'SuperCocktailsxa_111823.xml';
+
+    // Check if the XML file exists and is writable
+    if (file_exists($xmlFilePath) && is_writable($xmlFilePath)) {
+        // Load the XML file
+        $xml = simplexml_load_file($xmlFilePath);
+        if ($xml === false) {
+            echo "Failed to open XML file for reading.";
+            exit;
+        }
+
+        // Add a new cocktail entry
+        $cocktail = $xml->addChild('cocktail');
+        $cocktail->addChild('alcohol', $alcoholType);
+
+        // Save the updated XML back to the file
+        if ($xml->asXML($xmlFilePath) === false) {
+            echo "Failed to write to XML file.";
+        } else {
+            echo "Added alcohol type: " . $alcoholType;
+        }
+    } else {
+        if (!file_exists($xmlFilePath)) {
+            echo "Error: XML file does not exist.";
+        } else {
+            echo "Error: XML file is not writable.";
+        }
+    }
+} else {
+    // Message to show when accessing the file directly
+    echo "Please submit the form.";
+}
+?>
